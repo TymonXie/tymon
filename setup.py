170acf5c1a8b7bb22810cfec5baff82e5452bee4
TymonXie <847250484@qq.com>
2021-07-04 16:58:16 +0800

    add __init__.py in tools and setup.py
    Signed-off-by: TymonXie <847250484@qq.com>

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tymon",
+    version="0.0.2",
+    author="TymonXie",
+    author_email="847250484@qq.com",
+    description="An AI Assistant More Than a Toolkit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+ install_requires=[       # 工具包的依赖包
+    'scikit-learn>=0.24.2',
+    'torch>=1.9.0',
+     'pandas>=1.1.5',
+     'numpy>=1.19',
+     'matplotlib>=3.3.4',
+    ],
+    url="https://github.com/TymonXie/tymon",
+    packages=setuptools.find_packages(),
+    python_requires="~=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
